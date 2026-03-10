4aca7900f16070dd91d235200dfadaa9d2190bb8
Projects <kashishdiwakar3737@gmail.com>
2026-03-10 16:21:17 +0530

    Initial commit: Diebetes_prediction

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("diabetes_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+st.title("Diabetes Prediction App")
+
+preg = st.number_input("Pregnancies", min_value=0)
+glucose = st.number_input("Glucose", min_value=0)
+bp = st.number_input("Blood Pressure", min_value=0)
+skin = st.number_input("Skin Thickness", min_value=0)
+insulin = st.number_input("Insulin", min_value=0)
+bmi = st.number_input("BMI", min_value=0.0)
+dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0)
+age = st.number_input("Age", min_value=0)
+
+if st.button("Predict"):
+    data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
+    data = scaler.transform(data)
+    prediction = model.predict(data)
+    if prediction[0] == 1:
+        st.error("Person is likely to have Diabetes")
+    else:
+        st.success("Person is Not Diabetic")
